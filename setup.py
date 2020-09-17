cf34e923ab49b71199eb3a2b333a51dc6a2fdc55
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-09-16 21:38:22 -0700

    Adds a license.

--- setup.py
+++ setup.py
@@ -15,7 +15,20 @@ setuptools.setup(
     description=('A utility for performing basic text transformations.'),
     long_description=long_description,
     long_description_content_type='text/markdown',
+    project_urls={
+        'Source Code': 'https://github.com/aryann/xed',
+        "Author's Website": 'https://aryan.app',
+    },
     python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
     entry_points={
         'console_scripts': [
             'xed=xed.xed:cli',
